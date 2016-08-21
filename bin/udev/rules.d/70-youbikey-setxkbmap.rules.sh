aab50236f9f46348e2581df90552ea0882ccc92e
Ævar Arnfjörð Bjarmason <avarab@gmail.com>
2016-08-22 01:22:05 +0200

    udev: Just add to a logfile
    I'm going to pick this up in my dotfiles and run the setxkbmap command
    via screen.

--- bin/udev/rules.d/70-youbikey-setxkbmap.rules.sh
+++ bin/udev/rules.d/70-youbikey-setxkbmap.rules.sh
@@ -1,18 +1,3 @@
-#!/bin/sh -xe
+#!/bin/sh -e
 
-export PATH=/bin:/usr/bin
-export DISPLAY=:0
-DEVICE=$(xinput list | sed -n 's!.*Yubikey.*id=\([0-9]*\).*keyboard.*!\1!p')
-LOG=/tmp/udev.hook.log
-
-date >>$LOG
-sleep 2
-echo DEVICE=$DEVICE >>$LOG
-
-if test -z "$DEVICE"
-then
-    echo "Couldn't get a DEVICE"
-    exit 1
-fi
-
-setxkbmap -device $DEVICE -layout us >>$LOG 2>&1
+/bin/date >>/tmp/udev.70-youbikey-setxkbmap.hook
